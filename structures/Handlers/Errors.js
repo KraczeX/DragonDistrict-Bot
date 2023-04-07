7054114c8cabd16ea0b58fe19719156b505602dc
Krecik <bartoszkraczek2006@gmail.com>
2023-04-07 23:53:16 +0200

    update

--- structures/Handlers/Errors.js
+++ structures/Handlers/Errors.js
@@ -1,3 +1,48 @@
-const {Client, EmbedBuilder} = require("discord.js")
+const { Client, EmbedBuilder } = require("discord.js")
 const client = require("..")
 const ChannelID = process.env.LOGS
+
+
+/**
+ * @param {Client} client
+ */
+module.exports = async (client) => {
+
+    const Embed = new EmbedBuilder()
+        .setColor(client.color)
+        .setTimestamp()
+        .setFooter({ text: "Anty-crash by Krecik" })
+        .setTitle("Error")
+
+    process.on("unhandledRejection", (reason, p) =>{
+
+        console.log(reason, p)
+
+        const Channel = client.channels.cache.get(ChannelID)
+        if (!Channel) return
+
+        Channel.send({
+            embeds: [
+                Embed
+                .setDescription("**Unhandled rejection/catch:\n\n** ```" + reason +"```" )
+            ]
+        })
+    })
+
+    process.on("uncaughtException", (err, origin) =>{
+
+        console.log(err, origin)
+
+        const Channel = client.channels.cache.get(ChannelID)
+        if (!Channel) return
+
+        Channel.send({
+            embeds: [
+                Embed
+                .setDescription("**Unhandled rejection/catch:\n\n** ```" + reason +"```" )
+            ]
+        })
+    })
+}
+
+
