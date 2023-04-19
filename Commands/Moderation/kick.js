85e66207c55a962787e1c3edc3a0ce8b14e4f366
Krecik <bartoszkraczek2006@gmail.com>
2023-04-19 21:32:08 +0200

    update

--- Commands/Moderation/kick.js
+++ Commands/Moderation/kick.js
@@ -1,134 +1,117 @@
-const { Client, ChatInputCommandInteraction, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, ApplicationCommandOptionType} = require("discord.js")
-const ms = require("ms")
-const EditReply = require("../../Systems/editReply")
-const { execute } = require("../../Events/Client/ready")
-const Reply = require("../../Systems/reply")
-
+const { Client, CommandInteraction, MessageEmbed, MessageActionRow, MessageButton, ButtonInteraction } = require("discord.js");
+const ms = require("ms");
+const EditReply = require("../../Systems/editReply");
+const { execute } = require("../../Events/Client/ready");
+const Reply = require("../../Systems/reply");
 
 module.exports = {
-     name: "kick",
-     description: "Kicks a member from the server",
-     UserPerms: ["KickMembars"],
-     BotPerms: ["KickMember"],
-     category: "Moderation",
-     options: [
-          {
-               name: "user",
-               description: "Wybierz osobę",
-               type: 6,
-               required: true
-          },
-          {
-               name: "reason",
-               description: "Opisz powód",
-               type: 3,
-               required: false
-          }
-     ],
+  name: "kick",
+  description: "Wyrzuca użytkownika z serwera",
+  userPermissions: ["KICK_MEMBERS"],
+  botPermissions: ["KICK_MEMBERS"],
+  category: "Moderacja",
+  options: [
+    {
+      name: "użytkownik",
+      description: "Wybierz osobę, którą chcesz wyrzucić",
+      type: 6,
+      required: true,
+    },
+    {
+      name: "powód",
+      description: "Opisz powód wyrzucenia",
+      type: 3,
+      required: false,
+    },
+  ],
 
-     /**
-     *   @param { Client } client
-     *   @param {ChatInputCommandInteraction} interaction
-     */
-     async execute(interaction, client) {
+  /**
+   *   @param { Client } client
+   *   @param {CommandInteraction} interaction
+   */
+  async execute(interaction, client) {
+    await interaction.deferReply({ ephemeral: true });
 
-          await interaction.deferReply({ ephemeral: true })
+    const { options, user, guild } = interaction;
+    const member = options.getMember("użytkownik");
+    const reason = options.getString("powód") || "Nie podano powodu";
 
-          const { options, user, guild } = interaction
+    if (member.id === user.id) {
+      return EditReply(interaction, "❌ Nie możesz wyrzucić samego siebie.");
+    }
+    if (guild.ownerId === member.id) {
+      return EditReply(interaction, "❌ Nie możesz wyrzucić właściciela serwera.");
+    }
+    if (guild.me.roles.highest.position <= member.roles.highest.position) {
+      return EditReply(interaction, "❌ Nie możesz wyrzucić użytkownika z wyższą lub równą pozycją niż bot.");
+    }
+    if (interaction.member.roles.highest.position <= member.roles.highest.position) {
+      return EditReply(interaction, "❌ Nie możesz wyrzucić użytkownika z wyższą lub równą pozycją niż Ty.");
+    }
 
-          const member = options.getMember("user")
-          const reason = options.getString("reason") || "no reason provided"
+    const embed = new MessageEmbed().setColor(client.color);
+    const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setStyle("DANGER")
+        .setCustomId("kick-yes")
+        .setLabel("Tak"),
+      new MessageButton()
+        .setStyle("PRIMARY")
+        .setCustomId("kick-no")
+        .setLabel("Nie")
+    );
 
-          if (member.id === user.id) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
-          if (guild.ownerId === member.id) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
-          if (guild.members.me.roles.highest.position <= member.roles.highest.position) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
-          if (interaction.member.roles.highest.position <= member.roles.highest.position) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
+    const page = await interaction.editReply({
+      embeds: [embed.setDescription(`Czy na pewno chcesz wyrzucić użytkownika ${member}?`)],
+      components: [row],
+    });
 
-          const Embed = new EmbedBuilder()
-               .setColor(client.color)
+    const collector = page.createMessageComponentCollector({
+      componentType: "BUTTON",
+      time: ms("15s"),
+    });
 
-          const row = new ActionRowBuilder().addComponents(
+    collector.on("collect", async (i) => {
+      if (i.user.id !== user.id) return;
 
-               new ButtonBuilder()
-                    .setStyle(ButtonStyle.Danger)
-                    .setCustomId("kick-yes")
-                    .setLabel("Yes"),
+      switch (i.customId) {
+        case "kick-yes": {
+          await member.kick(reason);
 
-               new ButtonBuilder()
-                    .setStyle(ButtonStyle.Primary)
-                    .setCustomId("kick-no")
-                    .setLabel("No")
-          )
+          await interaction.editReply({
+            embeds: [embed.setDescription(`${member} został wyrzucony z serwera. Powód: **${reason}**.`)],
+            components: [],
+          });
 
-          const Page = await interaction.editReply({
+          try {
+            await member.send({
+              embeds: [
+                new MessageEmbed().setColor(client.color).setDescription(`Zostałeś wyrzucony z serwera ${guild.name}. Powód: ${reason}`),
+            ],
+            });
+            } catch (err) {
+            console.error(err);
+            }  break;
+        }
+    
+        case "kick-no": {
+          await interaction.editReply({
+            embeds: [embed.setDescription(`Anulowano wyrzucenie użytkownika ${member}.`)],
+            components: [],
+          });
+          break;
+        }
+    
+        default:
+          break;
+      }
+    });
+    
+    collector.on("end", async () => {
+      await interaction.editReply({
+        embeds: [embed.setDescription(`Czas na potwierdzenie wyrzucenia użytkownika ${member} minął.`)],
+        components: [],
+      });
+    }); },
+};
 
-               embeds: [
-                    Embed.setDescription(`** Napewno chcesz usunąć tego użytkownika? **`)
-               ],
-               components: [row]
-          })
-
-          const col = await Page.createMessageComponentCollector({
-               componentType: ComponentType.Button,
-               time: ms("15s")
-          })
-
-          col.on("collect", i => {
-
-               if (i.user.id !== user.id) return
-
-               switch (i.customId) {
-
-                    case "kick-yes": {
-
-                         member.kick({ reason })
-
-                         interaction.editReply({
-                              embeds: [
-                                   Embed.setDescription(`${member} został wyrzucony z serwera: **${reason}** `)
-                              ],
-                              components: []
-                         })
-
-                         member.send({
-                              embeds: [
-                                   new EmbedBuilder()
-                                        .setColor(client.color)
-                                        .setDescription(`Zostałeś wyrzucony z **${guild.name}**`)
-                              ]
-                         }).catch(err => {
-
-                              if (err.code !== 50007) return console.log(err)
-                         })
-
-                    }
-                         break;
-
-                    case "kick-no": {
-
-                         interaction.editReply({
-                              embeds: [
-                                   Embed.setDescription(`Polecenie kick zostało anulowane`)
-                              ],
-                              components: []
-
-                         })
-                    }
-                         break;
-               }
-          })
-
-          col.on("end", (collected) => {
-
-               if (collection.size >  0) return
-
-               interaction.editReply({
-                    embeds: [
-                         Embed.setDescription(`Nie dostarczyłeś poprawnej odpowiedzi na czas`)
-                    ],
-                    components: []
-               })
-          })
-
-     }
-}
---
+++ tak.js
@@ -0,0 +1,281 @@
+
+const { Client, ChatInputCommandInteraction, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, ApplicationCommandOptionType} = require("discord.js")
+const ms = require("ms")
+const EditReply = require("../../Systems/editReply")
+const { execute } = require("../../Events/Client/ready")
+const Reply = require("../../Systems/reply")
+
+
+module.exports = {
+     name: "kick",
+     description: "Kicks a member from the server",
+     UserPerms: ["KickMembars"],
+     BotPerms: ["KickMember"],
+     category: "Moderation",
+     options: [
+          {
+               name: "user",
+               description: "Wybierz osobę",
+               type: 6,
+               required: true
+          },
+          {
+               name: "reason",
+               description: "Opisz powód",
+               type: 3,
+               required: false
+          }
+     ],
+
+     /**
+     *   @param { Client } client
+     *   @param {ChatInputCommandInteraction} interaction
+     */
+     async execute(interaction, client) {
+
+          await interaction.deferReply({ ephemeral: true })
+
+          const { options, user, guild } = interaction
+
+          const member = options.getMember("user")
+          const reason = options.getString("reason") || "no reason provided"
+
+          if (member.id === user.id) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
+          if (guild.ownerId === member.id) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
+          if (guild.members.me.roles.highest.position <= member.roles.highest.position) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
+          if (interaction.member.roles.highest.position <= member.roles.highest.position) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
+
+          const Embed = new EmbedBuilder()
+               .setColor(client.color)
+
+          const row = new ActionRowBuilder().addComponents(
+
+               new ButtonBuilder()
+                    .setStyle(ButtonStyle.Danger)
+                    .setCustomId("kick-yes")
+                    .setLabel("Yes"),
+
+               new ButtonBuilder()
+                    .setStyle(ButtonStyle.Primary)
+                    .setCustomId("kick-no")
+                    .setLabel("No")
+          )
+
+          const Page = await interaction.editReply({
+
+               embeds: [
+                    Embed.setDescription(`** Napewno chcesz usunąć tego użytkownika? **`)
+               ],
+               components: [row]
+          })
+
+          const col = await Page.createMessageComponentCollector({
+               componentType: ComponentType.Button,
+               time: ms("15s")
+          })
+
+          col.on("collect", i => {
+
+               if (i.user.id !== user.id) return
+
+               switch (i.customId) {
+
+                    case "kick-yes": {
+
+                         member.kick({ reason })
+
+                         interaction.editReply({
+                              embeds: [
+                                   Embed.setDescription(`${member} został wyrzucony z serwera: **${reason}** `)
+                              ],
+                              components: []
+                         })
+
+                         member.send({
+                              embeds: [
+                                   new EmbedBuilder()
+                                        .setColor(client.color)
+                                        .setDescription(`Zostałeś wyrzucony z **${guild.name}**`)
+                              ]
+                         }).catch(err => {
+
+                              if (err.code !== 50007) return console.log(err)
+                         })
+
+                    }
+                         break;
+
+                    case "kick-no": {
+
+                         interaction.editReply({
+                              embeds: [
+                                   Embed.setDescription(`Polecenie kick zostało anulowane`)
+                              ],
+                              components: []
+
+                         })
+                    }
+                         break;
+               }
+          })
+
+          col.on("end", (collected) => {
+
+               if (collection.size >  0) return
+
+               interaction.editReply({
+                    embeds: [
+                         Embed.setDescription(`Nie dostarczyłeś poprawnej odpowiedzi na czas`)
+                    ],
+                    components: []
+               })
+          })
+
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const { Client, ChatInputCommandInteraction, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ApplicationCommandOptionType} = require("discord.js")
+const ms = require("ms")
+const EditReply = require("../../Systems/editReply")
+const execute = require("../../Events/Client/ready")
+
+module.exports = {
+     name: "ban",
+     description: "Bans a member from the server",
+     UserPerms: ["BanMembars"],
+     BotPerms: ["BanMembers"],
+     category: "Moderation",
+     options: [
+          {
+               name: "user",
+               description: "Wybierz osobę",
+               type: 6,
+               required: true
+          },
+          {
+               name: "reason",
+               description: "Opisz powód",
+               type: 3,
+               required: false
+          }
+     ],
+
+     /**
+     *   @param { Client } client
+     *   @param {ChatInputCommandInteraction} interaction
+     */
+     async execute(interaction, client) {
+
+          await interaction.deferReply({ ephemeral: true })
+
+          const { options, user, guild } = interaction
+
+          const member = options.getMember("user")
+          const reason = options.getString("reason") || "no reason provided"
+
+          if (member.id === user.id) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
+          if (guild.ownerId === member.id) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
+          if (guild.members.me.roles.highest.position <= member.roles.highest.position) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
+          if (interaction.member.roles.highest.position <= member.roles.highest.position) return EditReply(interaction, "❌" `Nie możesz wyrzucić tego użytkownika`)
+
+          const Embed = new EmbedBuilder()
+               .setColor(client.color)
+
+          const row = new ActionRowBuilder().addComponents(
+
+               new ButtonBuilder()
+                    .setStyle(ButtonStyle.Danger)
+                    .setCustomId("ban-yes")
+                    .setLabel("Yes"),
+
+               new ButtonBuilder()
+                    .setStyle(ButtonStyle.Primary)
+                    .setCustomId("ban-no")
+                    .setLabel("No")
+          )
+
+          const Page = await interaction.editReply({
+
+               embeds: [
+                    Embed.setDescription(`** Napewno chcesz usunąć tego użytkownika? **`)
+               ],
+               components: [row]
+          })
+
+          const col = await Page.createMessageComponentCollector({
+               componentType: ComponentType.Button,
+               time: ms("15s")
+          })
+
+          col.on("collect", i => {
+
+               if (i.user.id !== user.id) return
+
+               switch (i.customId) {
+
+                    case "ban-yes": {
+
+                         member.ban({ reason })
+
+                         interaction.editReply({
+                              embeds: [
+                                   Embed.setDescription(`${member} został wyrzucony z serwera: **${reason}** `)
+                              ],
+                              components: []
+                         })
+
+                         member.send({
+                              embeds: [
+                                   new EmbedBuilder()
+                                        .setColor(client.color)
+                                        .setDescription(`Dostałeś Bana na **${guild.name}**`)
+                              ]
+                         }).catch(err => {
+
+                              if (err.code !== 50007) return console.log(err)
+                         })
+
+                    }
+                         break;
+
+                    case "ban-no": {
+
+                         interaction.editReply({
+                              embeds: [
+                                   Embed.setDescription(`Polecenie ban zostało anulowane`)
+                              ],
+                              components: []
+
+                         })
+                    }
+                         break;
+               }
+          })
+
+          col.on("end", (collected) => {
+
+               if (collection.size >  0) return
+
+               interaction.editReply({
+                    embeds: [
+                         Embed.setDescription(`Nie dostarczyłeś poprawnej odpowiedzi na czas`)
+                    ],
+                    components: []
+               })
+          })
+
+     }
+}
+
+
